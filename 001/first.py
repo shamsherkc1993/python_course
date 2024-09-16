0aef5e5e20de416537fdb06536ab4bbf6b779dcc
shamsher <shamsher.kc.c4@gmail.com>
2024-09-16 12:33:59 +0900

    added first.py

---
+++ 001/first.py
@@ -0,0 +1,48 @@
+# name = input("Enter your name : " )
+# print("welocome", name)
+# print(type(name))
+
+# height = int(input("enter height : "))
+# width = int(input("enter width : "))
+
+# area = height * width
+# print("area is ", area)
+# print(type(height))
+
+
+# slicing 
+# name = "shamsher kc"
+# print(name[2: 4])
+# print(name[: 2])
+# print(name[2:])
+# print(name.endswith("d"))
+
+# age = 90
+
+# if(age >= 18  and age <= 80):
+#     print("can drive")
+# else: 
+#     print("cannot")
+
+# num1 = int(input("enter first number : "))
+# num2 = int(input("enter second number : "))
+# num3 = int(input("enter third number : "))
+# num4 = int(input("enter third number : "))
+
+
+# if(num1 > num2 and num1 > num3 and num4):
+#     print("num 1 is greater ", num1)
+# elif(num2 > num1 and num2 > num3 and num2 > num4):
+#     print("num 2 is greater ", num2)
+# elif(num3 > num1 and num3 > num2 and num3 > num4):
+#     print("num 3 is greater ", num3)
+# else:
+#     print("num 4 is greater ", num4)
+
+
+
+
+
+
+
+
